e06e7cd2fcf12a979e7e8a084645c2eff522d08f
AustrianMethod <ivanov.nv.spb@gmail.com>
2024-10-22 16:58:42 +0300

    done

---
+++ test.js
@@ -0,0 +1,15 @@
+import getState from './src/app.js';
+
+test('getState', () => {
+  const objH = {name: 'Маг', health: 90};
+  const objW = {name: 'Маг', health: 40};
+  const objC = {name: 'Маг', health: 10};
+  
+  const resultH = getState(objH);
+  const resultW = getState(objW);
+  const resultC = getState(objC);
+
+  expect(resultH).toBe('healthy');
+  expect(resultW).toBe('wounded');
+  expect(resultC).toBe('critical');
+});
