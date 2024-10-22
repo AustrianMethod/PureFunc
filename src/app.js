71eff0e0be62e53cc1c94d4209496296b9b173ac
AustrianMethod <ivanov.nv.spb@gmail.com>
2024-10-22 17:58:37 +0300

    fixed lint bugs

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 export default function getState(obj) {
-  if ( obj.health > 50 ) {
+  if (obj.health > 50) {
     return 'healthy';
-  } else if ( obj.health >= 15 && obj.health <= 50 ) {
+  } if (obj.health >= 15 && obj.health <= 50) {
     return 'wounded';
-  } else if ( obj.health < 15 ) {
+  } if (obj.health < 15) {
     return 'critical';
   }
+  return 'ни одно условие не выполнено';
 }
--- test.js
+++ test.js
@@ -1,10 +1,10 @@
-import getState from './src/app.js';
+import getState from './src/app';
 
 test('getState', () => {
-  const objH = {name: 'Маг', health: 90};
-  const objW = {name: 'Маг', health: 40};
-  const objC = {name: 'Маг', health: 10};
-  
+  const objH = { name: 'Маг', health: 90 };
+  const objW = { name: 'Маг', health: 40 };
+  const objC = { name: 'Маг', health: 10 };
+
   const resultH = getState(objH);
   const resultW = getState(objW);
   const resultC = getState(objC);
